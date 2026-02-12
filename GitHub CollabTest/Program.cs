99b90eb1434d71c08f31fe4df5f709f8e55c3558
Dully04 <abs004@edu.zealand.dk>
2026-02-12 13:30:36 +0100

    sko tilføjet

--- GitHub CollabTest/Program.cs
+++ GitHub CollabTest/Program.cs
@@ -11,5 +11,5 @@ Console.WriteLine("10. forsøg");
 Console.WriteLine("Endnu et merge forsøg");
 Console.WriteLine("10. forsøg");
 Console.WriteLine("heya!");
-Console.ReadLine("sko");
+Console.WriteLine("sko");
 Console.ReadLine();
